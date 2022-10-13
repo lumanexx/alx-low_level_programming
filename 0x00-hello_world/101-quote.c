e85ea2f3858bcac78168c1ba27b3fcf87732c567
lumanexx <techlumanex@gmail.com>
2022-10-13 13:21:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Decription: fprintf function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+	return (1);
+}

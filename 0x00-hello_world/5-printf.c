ff01029b2d7a1be2887e7b0d479262aeb7384ad4
lumanexx <techlumanex@gmail.com>
2022-10-13 10:35:06 -0700

    5-p

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print grammer
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

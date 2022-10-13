0e40a91591d3ced78307a135f9f7751c43902ee2
lumanexx <techlumanex@gmail.com>
2022-10-13 08:22:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -to print Programming is like building a multilingual puzzle
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}

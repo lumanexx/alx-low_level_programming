e7b6c9e991bcca294319ed18c759268cfd97ee61
lumanexx <techlumanex@gmail.com>
2022-10-13 10:12:20 -0700

    4-p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Description: 'the descrition of the program'
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
-{  
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
+
